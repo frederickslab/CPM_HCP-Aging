ce8ed8e9335de10bc4e4a4405a36dfadfbc884b5
sju777 <suyeon.ju@yale.edu>
2022-10-04 17:17:26 -0400

    update script ot calculate association ratios to concatenate conn mats within the script now

---
+++ association_ratio_analyses.m
@@ -0,0 +1,135 @@
+close all
+% clear
+% clc
+
+%% conn mat stuff
+% 1. use the individual connectivity matrix .mat file to make stacked (3D)
+% matrices in this script run's workspace
+% 2. run the association ratio script on each stacked matrix
+% 3. somehow collect all of the association ratio values and run t-tests
+
+%% load the 268 atlas based connectivity matrices
+data_path = '/Users/sj737/Library/CloudStorage/OneDrive-YaleUniversity/Fredericks_Lab_files/CPM_HCP-A/BIG_data_from_CPM_HCP-Aging/';
+load([data_path, 'all_conn_mats_indiv.mat'], 'conn_mat_struct_all');
+load([data_path, 'pt_id_connmat_grouped.mat'], 'pt_id_connmat_struct');
+
+scan_type_list = {'rfMRI_REST1_AP', 'rfMRI_REST1_PA', 'rfMRI_REST2_AP', 'rfMRI_REST2_PA','tfMRI_CARIT', 'tfMRI_FACENAME', 'tfMRI_VISMOTOR'};
+
+for s = 1:length(scan_type_list)
+    [mat_all, mat_F, mat_M] = concat_connmats(conn_mat_struct_all, pt_id_connmat_struct, scan_type_list{s});
+    
+    % compute association ratios for each group
+    a_ratio_all = compute_association_ratio(mat_all);
+    a_ratio_F = compute_association_ratio(mat_F);
+    a_ratio_M = compute_association_ratio(mat_M);
+    
+%     % plot association ratios of each group
+%     plot_association_ratio_hm(a_ratio_all, scan_type_list{s}, s, 'all');
+%     plot_association_ratio_hm(a_ratio_F, scan_type_list{s}, s,'F');
+%     plot_association_ratio_hm(a_ratio_M, scan_type_list{s}, s,'M');
+
+    % PERFORM T-TESTS HERE!!!
+    
+end
+
+%% fxn to concatenate all existing conn mats for each input scan type name
+function [all_mat, F_mat, M_mat] = concat_connmats(connmat_struct, pt_id_struct, scan_type_name)
+    % get all pt id's for each subj group
+    fn_all = pt_id_struct.(char(scan_type_name)).all_subs;
+    fn_F = pt_id_struct.(char(scan_type_name)).F_subs;
+    fn_M = pt_id_struct.(char(scan_type_name)).M_subs;
+    
+    % initialize matrices for each group
+    all_mat = zeros(268,268,size(fn_all,1));
+    F_mat = zeros(268,268,size(fn_F,1));
+    M_mat = zeros(268,268,size(fn_M,1));
+    
+    % fill in matrices for each group
+    for s = 1:length(fn_all)
+        all_mat(:,:,s) = connmat_struct.(char(scan_type_name)).(char(fn_all{s}));
+    end
+    for s = 1:length(fn_F)
+        F_mat(:,:,s) = connmat_struct.(char(scan_type_name)).(char(fn_F{s}));
+    end
+    for s = 1:length(fn_M)
+        M_mat(:,:,s) = connmat_struct.(char(scan_type_name)).(char(fn_M{s}));
+    end
+end
+
+%% fxn to plot heatmap of association ratios across all networks for each scan type
+function plot_association_ratio_hm(A_ratio, scan_type_name, scan_type_num, group_name)
+    switch scan_type_num
+            case 1
+                scan_type = 'rfMRI\_REST1\_AP';
+            case 2
+                scan_type = 'rfMRI\_REST1\_PA';
+            case 3
+                scan_type = 'rfMRI\_REST2\_AP';
+            case 4
+                scan_type = 'rfMRI\_REST2\_PA';
+            case 5
+                scan_type = 'tfMRI\_CARIT';
+            case 6
+                scan_type = 'tfMRI\_FACENAME';
+            case 7
+                scan_type = 'tfMRI\_VISMOTOR';
+    end
+
+    fig = figure;
+    set(gcf, 'Position', [0, 0, 500, 1500]);
+    
+    hm = heatmap(A_ratio,'Colormap', jet);
+    hm.Title = [scan_type];
+    hm.XDisplayLabels = {'MF','FP','DMN','Mot','VI','VII','VAs','SAL','SC','Cer'};
+    hm.XLabel = '10 networks';
+    hm.YLabel = 'subject number';
+    
+    hm_filename = sprintf('../cpm_figures/network_segregation_heatmaps/netseg_hm_%s_%s.png', scan_type_name, group_name);
+    saveas(fig,hm_filename) 
+end
+
+%% fxn to compute association ratio
+function ass_ratio = compute_association_ratio(mat_all)
+    % set up variables for number of subjects, nodes, and networks
+    no_sub = size( mat_all, 3);
+    no_nodes = size( mat_all, 1);
+    no_net = 10;
+    
+    % load the network labels for the 268 parcellation
+    % first column is the ROI index
+    % second column is the 10 network labels (full network names commented below)
+    % convert the two column lookup table to 1D label
+    %     1 = medial frontal
+    %     2 = frontoparietal
+    %     3 = default mode
+    %     4 = motor cortex, 
+    %     5 = visual A
+    %     6 = visual B
+    %     7 = visual association
+    %     8 = salience
+    %     9 = subcortical
+    %     10 = cerebellum
+    look_table = dlmread('Shen268_10network');
+    
+    % set sigma ***SUYEON, FIGURE OUT WHAT THIS IS EXACTLY...
+    sigma = mean( 1- mat_all(1:50:end));
+    
+    % 
+    label = zeros(1, no_nodes);
+    for i = 1:no_net
+        cur_id= find( look_table(:,2) ==i);
+        label( look_table(cur_id,1)) = i;
+    end
+    
+    % initialize association ratio matrix (no_sub x no_net)
+    ass_ratio = zeros(no_sub, no_net);
+    
+    % fill in association ratio matrix
+    for i = 1:no_sub
+        cur_mat = squeeze( mat_all(:,:,i));
+        ass_ratio(i, :) = association( cur_mat, sigma, label);
+    end
+end
+
+%% fxn to run t-tests on association ratios between sexes
+
